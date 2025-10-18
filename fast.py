5ba231ef02bb086a4ea8ad3c30b3bd9609df98e4
Nkwam Philip <philipnkwam@gmail.com>
2025-10-18 03:24:37 +0100

    Change Uvicorn workers from 16 to 1
    Reduce the number of workers for the Uvicorn server from 16 to 1.

--- fast.py
+++ fast.py
@@ -275,4 +275,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=16)
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
